d017a882483451f516d4534e971753c98070aced
l1kaa <chxikvadzelikuna1@gmai.com>
2025-01-26 17:10:05 +0400

    ★

---
+++ day 57/classwork/script.js
@@ -0,0 +1,8 @@
+// js-ში ვისწავლეთ 2 მონაცემთა ტიპი:
+// Number - რიცხვი
+// String - ბრჭყალებში მოქცეული მონაცემი
+
+// DOM-ის სრული ვერსია არის Document Object Model. მისი საშუალებით HTML-დან js-ში გადმოგვაქვს სასურველი ელემენტი, რის შემდეგადაც უკვე js-ში შეგვიძლია ამ ელემენტის მოდიფიკაცია.
+
+let element = document.getElementById('myHeading');
+console.log(element);
---
+++ day 57/homework/script.js
@@ -0,0 +1,21 @@
+// 2)  მოიძიეთ ინფორმაცია Document Object Model'ზე და ახსენით რა არის
+
+// The DOM is a W3C (World Wide Web Consortium) standard.
+// The DOM defines a standard for accessing documents:
+// "The W3C Document Object Model (DOM) is a platform and language-neutral interface that allows programs and scripts to dynamically access and update the content, structure, and style of a document."
+// The W3C DOM standard is separated into 3 different parts:
+
+// Core DOM - standard model for all document types
+// XML DOM - standard model for XML documents
+// HTML DOM - standard model for HTML documents
+
+// 3) შექმენით 2 Number ტიპის ცვლადი, დაამატეთ ერთმანეტს და გამოიტანეთ console'ში
+let num1 = 2000
+let num2 = 350
+console.log(num1 + num2)
+
+// 4) ახსენით რა არის console
+// console - ის საშუალებით ჩვენ შეგვიძლია ჩვენს მიერ დაწერილი Javascript-ის კოდი დავინახოთ. ამასთანავე, console გამოიყენება debugging-ისთვისაც.
+
+// 5) ახსენით რას აკეთებს let keyword
+// let keyword-ის საშუალებით ჩვენ ვახდენთ ცვლადის დეკლარირება. 
