2deebdde7a607d218e6601aece340761048e1a51
l1kaa <chxikvadzelikuna1@gmai.com>
2025-02-01 20:10:29 +0400

    ★

---
+++ day 59/homework/script.js
@@ -0,0 +1,9 @@
+const numInput1 = prompt("enter the first number: ");
+const numInput2 = prompt("enter the second number: ");
+console.log(numInput1 + numInput2)
+
+const wordInput1 = prompt("enter a word: ")
+const wordInput2 = prompt("enter a second word: ")
+console.log(wordInput1 + wordInput2)
+
+// საკლასოს მეოთხე დავალებაში name ატრიბუტები გვჭირდება იმისთვის, რომ Javascript-ის კოდს მივცეთ წვდომა HTML-ის დოკუმენტში არსებულ input-ებზე.
