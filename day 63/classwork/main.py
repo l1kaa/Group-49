c973a65e12edcdb8675c2555ec13985b43b0da21
l1kaa <chxikvadzelikuna1@gmai.com>
2025-02-09 16:26:07 +0400

    ★

---
+++ day 63/classwork/main.py
@@ -0,0 +1,12 @@
+age = input("enter your age: ")
+
+if age < 13:
+    print("You are a kid.")
+elif age > 13 and age < 18:
+    print("You are not an adult yet.")
+else:
+    print("You are an adult.")
+
+# js-ისა და Python-ის If/else statement-ებში შემდეგი სინტაქქსური განსხვავებებია:
+# js-ში if/else/else if statement-ების შემდეგ მოცემული პირობა უნდა ჩავსვათ ფრჩხილებში და პითონისგან განსხვავებით, ორი წერტილის (:) მაგივრად ფიგურული ფრჩხილები უნდა დავწეროთ.
+# ამასთანავე, პითონში ვიყენებთ elif-ს, ხოლო Js-ში ვიყენებთ else if-ს.
---
+++ day 63/classwork/script.js
@@ -0,0 +1,29 @@
+//1) მომხმარებელს შემოატანინეთ რიცხვი და უთხარით მისი grade (ანუ თუ მაგალითად
+//  90'დან 100'ის ჩათვლით ექნება ქულა გამოუტანეთ "Grade A", თუ 80'დან 89'ის ჩათვლით გამოუტანეთ "Grade B" და ა.შ)
+
+const scoreInput = prompt("enter your score: ");
+console.log(scoreInput)
+if(100 >= scoreInput < 90 ){
+    alert("Grade A");
+} else if(89 >= scoreInput < 80 ){
+    alert("Grade B");
+} else if(70 >= scoreInput < 79 ){
+    alert("Grade C");
+} else {
+    alert("Grade F");
+}
+
+
+// 2) მომხმარებელს შემოატანინეთ საკუთარი ასაკი, თუ იქნება 13 წელზე ნაკლების გამოუტანეთ You are kid, თუ იქნება 18 წელზე ნაკლების მაგრამ 13'ზე მეტის გამოუტანეთ You are not adult yet და თუ იქნება 18 წელზე მეტის გამოუტანეთ You are adult
+
+const ageInput = prompt("enter your age: ")
+console.log(ageInput)
+
+if (ageInput < 13){
+    alert("You are a kid.");
+} else if(ageInput < 18 && ageInput > 13){
+    alert("You are not an adult yet.");
+} else if(ageInput > 18){
+    alert("You are an adult.");
+}
+
