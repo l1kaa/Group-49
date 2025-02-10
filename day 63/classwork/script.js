ed390331b5cbde162126719756e46fa5db3d19e4
l1kaa <chxikvadzelikuna1@gmai.com>
2025-02-10 20:15:22 +0400

    ★

--- day 63/classwork/script.js
+++ day 63/classwork/script.js
@@ -3,11 +3,11 @@
 
 const scoreInput = prompt("enter your score: ");
 console.log(scoreInput)
-if(100 >= scoreInput < 90 ){
+if(100 <= scoreInput < 90 ){
     alert("Grade A");
-} else if(89 >= scoreInput < 80 ){
+} else if(89 <= scoreInput < 80 ){
     alert("Grade B");
-} else if(70 >= scoreInput < 79 ){
+} else if(70 <= scoreInput < 79 ){
     alert("Grade C");
 } else {
     alert("Grade F");
