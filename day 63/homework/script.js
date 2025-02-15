2fcfff0b44a8e7f63d4103b36b0a2d6f8d2da758
l1kaa <chxikvadzelikuna1@gmai.com>
2025-02-15 11:05:07 +0400

    ★

--- day 63/homework/script.js
+++ day 63/homework/script.js
@@ -1,23 +1,23 @@
 //  შექმენით while loop'ი რომელიც გამოიტანს კენტ რიცხვებს 1'დან 200'მდე
-x = 1
+let x = 1;
 while(x < 200){
     console.log(x);
     x+=2
 }
 
 //შექმენით while loop'ი რომელიც გამოიტანს 3'ის ჯერად რიცხვებს 51'დან 150'მდე
-y = 51
+let y = 51;
 while(y < 100){
-    console.log(y)
+    console.log(y);
     y+=3
 }
 
 // შექმენით for loop'ი რომელიც გამოიტანს ყველა რიცხვს 0'დან 100'მდე
 for (let i = 0; i < 100; i++){
-    console.log(i)
+    console.log(i);
 }
 
 // შექმენით for loop'ი რომელიც გამოიტანს ყოველ მეორე რიცხვს 100'დან 300'მდე 
 for (let n = 100; n < 300; n+2){
-    console.log(n)
+    console.log(n);
 }
