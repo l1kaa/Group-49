7cbb325fd4bebb296fcbb84642aa1ea2c5399ec8
l1kaa <chxikvadzelikuna1@gmail.com>
2025-02-18 16:50:48 +0400

    ★

--- day 65/homework/script.js
+++ day 65/homework/script.js
@@ -1,2 +1,27 @@
-//
-//
+//შექმენით Object Constructor ნებისმიერ რამეზე რომელსაც ექნება 5 property 
+
+const obj1 = {
+    favColor : "Black",
+    favDrink : "Water",
+    post1 : "English mentor",
+    post2: "Mentor's assistent",
+    post3: "Leader"
+}
+
+console.log(obj1)
+
+//შექმენით Object Constructor მოტოებზე, უნდა ჰქონდეს 4 property და ერთ-ერთი უნდა იყოს მეთოდი
+
+function Motorcycle(brand, name, color, method){
+    this.brand = brand;
+    this.name = name;
+    this.color = color;
+    this.method = method;
+}
+
+function switchColor(){
+    this.color = "Blue"
+}
+
+const Motorcycle1 = new Motorcycle("Yamaha", "Yamaha motor R9", "Black", switchColor)
+console.log(Motorcycle1)
