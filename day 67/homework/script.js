7e73e60ed5f80b32624ffd0a2a40c80ab2305d4c
l1kaa <chxikvadzelikuna1@gmail.com>
2025-02-23 16:02:41 +0400

    ★

--- day 67/homework/script.js
+++ day 67/homework/script.js
@@ -13,17 +13,46 @@ console.log(sumArr([720, 54, 90 ,4 ,14]))
 
 // 2)შექმენით ფუნქცია, რომელიც მიიღებს რიცხვების მასივს და დააბრუნებს მის მინიმალურ და მაქსიმალურ მნიშვნელობებს.
 
-function minMax(numArray){
-    return 
+function minMax(arr){
+    let minNum = arr[0]
+
+    for(let i = 0; i < arr.length; i++){
+        if (arr[i] < minNum){
+            minNum = arr[i]
+        }
+    }
 }
+
+    let maxNum = arr [0]
+    for(let i = 0; i < arr.length; i++){
+        if (arr[i] > maxNum){
+            maxNum = arr[i]
+        }
+    }
+
+return [minNum, maxNum]
 
 
 // 3)დაწერეთ ფუნქცია, რომელიც შექმნის N სიგრძის მასივს შემთხვევითი რიცხვებით (1-100 შუალედში).
-let objects = new Array(100)
+function randomArr(){
+    let num1 = Math.ceil(Math.random() * 100)
+    let num2 = Math.ceil(Math.random() * 100);
+    const arr =  [];
+
+    for(let i = Math.min(num1,num2); i < Math.max(num1,num2); i++){
+        arr.push(i)
+    }
+    return arr
+}
 
 // 4)შექმენით ფუნქცია, რომელიც მიიღებს რიცხვების მასივს და დააბრუნებს ახალ მასივს, სადაც ყველა ელემენტი იქნება მისი კვადრატი.
-function squared(Number){
-    
+function square(arr){
+    const newArr = [];
+
+    for(let i = 0; i < arr.lenght; i++){
+        newArr.push(arr[i] * arr[i])
+    }
+    return newArr;
 }
 
 
