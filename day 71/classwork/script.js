79d0f021e92f3018e7f092b4b4ab3783aa69ad12
l1kaa <chxikvadzelikuna1@gmail.com>
2025-03-01 12:44:55 +0400

    ★

--- day 71/classwork/script.js
+++ day 71/classwork/script.js
@@ -10,15 +10,15 @@ let obj3 = document.querySelector("#myp")
 // 3) წამოიღეთ js'ში img და შეუცვალეთ: src და width
 
 let changeImage = document.getElementsByTagName("img")
-img.src = "./img1.jpg"; 
-img.width = 500; 
+changeImage.src = "./img1.jpg"; 
+changeImage.width = 500; 
 
 // 4) წამოიღეთ js'ში p და შეუცვალეთ: color, backgroundColor და fontSize
 
 let mypChange = document.getElementById("myp")
-p.style.color = "blue"; 
-p.style.backgroundColor = "black"; 
-p.style.fontSize = "25px";
+mypChange.style.color = "blue"; 
+mypChange.style.backgroundColor = "black"; 
+mypChange.style.fontSize = "25px";
 
 
 // 5) js'ის გამოყენებით, შექმენით ახალი p და textNode, შემდეგ textNode ჩასვით პარაგრაფში და პარაგრაფი ჩასვით html'ში მოცემულ div'ში
