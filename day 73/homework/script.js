92147c3c521d6b81524283b0b3a9f4aae8da1a81
l1kaa <chxikvadzelikuna1@gmail.com>
2025-03-06 18:07:16 +0400

    ★

---
+++ day 73/homework/script.js
@@ -0,0 +1,3 @@
+// scoping in js determines the range of using certain variables/variable names outside of a js code block. there are various types of scopes in js. for example: Global scope, function scope and block scope.
+
+// with the use of hoisting, the engine of js in browsers automatically moves up a declaration of a variable to the very top of the code, which depends on the priority of a variable.
