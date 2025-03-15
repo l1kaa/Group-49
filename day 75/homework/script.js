03d60b787861ebf281604ec89ba4af07ee78af00
l1kaa <chxikvadzelikuna1@gmail.com>
2025-03-15 10:04:02 +0400

    ★

--- day 75/homework/script.js
+++ day 75/homework/script.js
@@ -1,1 +1,31 @@
-// hw
+const child = document.getElementById('child');
+
+let x = 0;
+let y = 0;
+moveAmount = 5
+
+document.addEventListener("keydown", event => {
+
+    if(event.key.startsWith("Arrow")){
+
+        event.preventDefault();
+
+        switch(event.key){
+            case "ArrowUp":
+                y -= moveAmount;
+                break;
+            case "ArrowDown":
+                y += moveAmount;
+                break;   
+            case "ArrowRight":
+                x += moveAmount
+                break
+            case "ArrowLeft":
+                x -= moveAmount;
+                break
+        }
+
+        child.style.top = `${y}px`;
+        child.style.left = `${x}px`;
+    }
+})
