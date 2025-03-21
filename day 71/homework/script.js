dcc67b644f08448932ae51b408ee4d065ef2afe4
l1kaa <chxikvadzelikuna1@gmail.com>
2025-03-21 11:15:12 +0400

    ★

--- day 71/homework/script.js
+++ day 71/homework/script.js
@@ -1,1 +1,21 @@
-// frotend mentor
+const images = ["./assets/images/icon-minus.svg", "./assets/images/icon-plus.svg"];
+
+const btns = document.getElementsByClassName("btn-img");
+
+const func = (target) => {
+    const p = target.parentElement.nextElementSibling;
+    if(p.style.display === "block") {
+        p.style.display = "none";
+        target.src = images[1];
+    } else {
+        p.style.display = "block";
+        target.src = images[0];
+    }
+}
+
+
+for(let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", (e) => {
+        func(e.target);
+    })
+}
