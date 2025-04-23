684605b357301121e034dd644b9afa9513e23e88
l1kaa <chxikvadzelikuna1@gmail.com>
2025-04-23 20:46:05 +0400

    ★

---
+++ day 93/homework/main.py
@@ -0,0 +1,64 @@
+# Q&A
+# 1) Is str data type mutable or immutable?
+# str data type is immutable, because we can't change it no matter what (except for using replace method.)
+
+# 2) If str is immutable then how does .replace() method work? We are able to mutate our string
+# when we use .replace() it doesn't actually change the string itself. it produces another string.
+
+# 3) Are all collection types matched to their bracket types? list: [], tuple: {}, dict: {}, set: ()
+# No. The correct way would be: list: [], tuple: (), dict: {}, set: {}
+
+# 4) Is sentence "I created function" syntactically correct or not?
+# No it't not syntactically correct. The correct way would be I defined a function.
+
+# 5) If python is case-sensitive language...
+# It means that "A" and "a" is treated differently.
+
+# 6) What will print(True + True) output?
+# It's gonna output 2, becaus True equals 1. Therefore (1 + 1 = 2).
+
+# 7) What will print(int("1_000_000")) output?
+# It's gonna Output 1_000_000. It is so we can easily tell what the numbers are.
+
+# 8) What will this code output?
+# კოდი არ არის ნაჩვენები, შესაბამისად კითხვას ვერ გავცემ პასუხს.
+
+# 9) Is empty list Truthy or Falsey value?
+# empty list is a Falsey value. Therefore a list containing elements is Truthy value.
+
+# 10) What will this code output?
+# კოდი არ არის ნაჩვენები, შესაბამისად კითხვას ვერ გავცემ პასუხს.
+
+# 11) Can we create list comprehension with if statement?
+# Yes we can. for example: return "even" if n % 2 == 0
+
+# 12) Can we create list comprehension with if-else statement?
+# we can. for example: return "even" if n % 2 == 0 else "odd"
+
+# 13) Can you define function inside function?
+# Yes, that's possible. For example:
+
+def hello():
+    def goodbye():
+        return "Goodbye"
+
+# 14) Is there error type called "ZeroDivisionError"?
+# Yes, There is. It pops up whenever we try to divide a number by Zero.
+
+# 15) What are lambda functions called?
+# lambda functions are called "Anonymous Functions"
+
+# 16) What are lambda functions called?
+# Still "Anonymous Functions"
+
+# 17) What will print(not "") output?
+# print(not "") will output True, since print("") would be False. (an empty string is always a False value)
+
+# 18) If someone said "Everything is an Object in python", would it be a lie?
+# No, that statement would be absolutely correct.
+
+# 19) Are "is" and "==" operators both exactly the same in python?
+# "is" and "==" operators aren't exactly the same in python.
+
+# 20) Is python high level or low level programming language?
+# python is a high level programming language, which means that it's far from machine language.
